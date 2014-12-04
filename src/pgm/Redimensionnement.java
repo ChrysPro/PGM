573dd4138298cf5a43ff719cc8100a1e8c93f51f
ybellehi <ybellehi@pc-b13-07.eleves.ecn>
2014-12-04 08:51:41 +0100

    Classe Redimensionnement fini(ou presque)

--- src/pgm/Redimensionnement.java
+++ src/pgm/Redimensionnement.java
@@ -24,13 +24,17 @@ public class Redimensionnement {
         int r=newHauteur/image.getLongueur();//Ratio de redimensionnement
         int a=0;//1-reste à utiliser de l'image à redimensionner
         int b=0;//ligne courante de l'image à redimensionner
-        Read newImage = new Read ("redim");//image redimensionné
+        Read newImage = new Read ("redimLongueur");//image redimensionné
         ArrayList<Integer> newTableau=null;
         
         for(int i=0;i<newHauteur;i++){
             
             this.parLigne(image, r, a, b, i, newTableau);
         }
+        
+        newImage.setTableau(newTableau);
+        newImage.setLargeur(image.getLargeur());
+        newImage.setLongueur(newHauteur);
         
     }
     
@@ -45,8 +49,62 @@ public class Redimensionnement {
                 d=1-c;
             }
             
-            for(int j=0+; j<image.getLargeur();j++){
-                newTableau.add(j,image.getTableau().get(j)*d);
+            for(int j=0; j<image.getLargeur();j++){
+                newTableau.add(j+i*image.getLargeur(),image.getTableau().get(j+b*image.getLargeur())*d);
+                c=c+d;
+                a=a+d;
+            }
+            if(a==r){
+                b++;
+            }
+            
+        }
+    }
+    
+    public void setLargeur(int newLargeur){
+        
+        int r=newLargeur/image.getLargeur();//Ratio de redimensionnement
+        int a=0;//1-reste à utiliser de l'image à redimensionner
+        int b=0;//colonne courante de l'image à redimensionner
+        Read newImage = new Read ("redimLargeur");//image redimensionné
+        ArrayList<Integer> newTableau=null;
+        for(int i=0;i<image.getLongueur()*newLargeur;i++){
+            newTableau.add(i,0);
+        }
+        
+        for(int i=0;i<newLargeur;i++){
+            
+            this.parColonne(image, r, a, b, i, newTableau);
+        }
+        
+        newImage.setTableau(newTableau);
+        newImage.setLargeur(newLargeur);
+        newImage.setLongueur(image.getLongueur());
+        
+    }
+    
+    
+    public void parColonne(Read image, int r, int a, int b, int i, ArrayList newTableau){
+        int c=0;//% de remplissage de la nouvelle ligne
+        int d;
+        
+        while(c!=1){
+            if(r<=1-c){
+                d=r;
+            }else{
+                d=1-c;
+            }
+            
+            for(int j=0; j<image.getLongueur();j++){
+                newTableau.add(j*image.getLongueur()+i,image.getTableau().get(j*image.getLongueur()+b)*d);
+                c=c+d;
+                a=a+d;
+            }
+            
+            
+            
+            if(a==r){
+                b++;
             }
             
         }
